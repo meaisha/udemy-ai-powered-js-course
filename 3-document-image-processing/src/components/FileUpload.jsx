2b77eb4f60e43b094722e3c76ece20dd6b304d0f
Aisha Agivetova <meaisha@gmail.com>
2025-11-06 13:10:30 -0500

    Adds file upload functionality

--- 3-document-image-processing/src/components/FileUpload.jsx
+++ 3-document-image-processing/src/components/FileUpload.jsx
@@ -1,11 +1,24 @@
+import {Buffer} from 'buffer';
+
+
 function FileUpload() {
+
+    async function handleFileUpload(event) {
+        const fileUpload = await event.target.files[0].arrayBuffer();
+        const file = {
+            type: event.target.files[0].type,
+            file: Buffer.from(fileUpload).toString,
+        };
+        console.log("File uploaded:", file);
+        // Add further processing logic here
+    }
   return (
     <section>
         <h2>Get Started</h2>
         <input 
             type="file" 
-            name="" 
-            id="" 
+            accept=".pdf, .jpeg, .jpg, .png" 
+            onChange={handleFileUpload}
         />
     </section>
   );
